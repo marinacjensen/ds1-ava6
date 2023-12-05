68edccd8797e1d76da4a2a4cf17f296d7c63d632
marinacjensenn <84468498+marinacjensenn@users.noreply.github.com>
2023-12-05 18:22:28 -0300

    finalização do projeto

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import { useState } from 'react'
 
 function App() {
-  const [ city, setCity] = useState("")
+  const [ city, setCity] = useState('')
+  const [weather, setWeather] = useState(null)
+
+  const changes = (event) => {
+    setCity(event.target.value)
+  }
+
+  const search = () => {
+    fetch(`http://api.weatherapi.com/v1/current.json?key=e751ea9cf5bc4fcbbb3225428211810&q=${city}&lang=pt`)
+    .then((resp) => {
+      if(resp.status === 200) {
+        return resp.json()
+      }
+    })
+    .then((data) => {
+      console.log(data)
+      setWeather(data);
+    })
+  }
 
   return (
     <div>
@@ -20,11 +38,48 @@ function App() {
           </p>
 
           <div className="row mb-4">
-            <div className="col-md-6">
-              <input className="form-control" placeholder='Insira aqui o nome da cidade' value={city}></input>
+            <div className="col-md-6 d-flex">
+              <input className="form-control" placeholder='Insira aqui o nome da cidade' value={city} onChange={changes}></input>
+              <button className = "btn btn-dark ml-2" onClick = {search}>Pesquisar</button>
             </div>
           </div>
-          <button className = "btn btn-dark btn-lg" >Pesquisar</button>
+          
+          {
+            weather ?(
+              <div className='mt-2'>
+                <div className = 'mt-6 d-flex align-items-center'>
+                  <div>
+                    <img src = {weather.current.condition.icon}/>
+                  </div>
+                  <div>
+                    <h4>Clima de hoje: {weather.current.condition.text}</h4>
+                    <p className='lead'>Temperatura atual: {weather.current.temp_c}°C</p>
+                  </div>
+                </div>
+                <div className='align-items-center mt-2'>
+                  <table className='table'>
+                      <thead className='thead-dark'>
+                        <tr>
+                          <th scope='col'>Pressão</th>
+                          <th scope='col'>Nuvens (%)</th>
+                          <th scope='col'>Sensação Térmica</th>
+                          <th scope='col'>Umidade</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td>{weather.current.pressure_mb}mbar</td>
+                          <td>{weather.current.cloud}%</td>
+                          <td>{weather.current.feelslike_c}°C</td>
+                          <td>{weather.current.humidity}%</td>
+                        </tr>
+                      </tbody>
+                  </table>
+                </div>
+              </div>
+            ) : null
+          }
+
         </div>
       </div>
     </div>
